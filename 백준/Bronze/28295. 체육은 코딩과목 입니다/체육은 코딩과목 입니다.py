0f41e9be4d10e7ad19689dc44b41a370b217d716
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2025-12-14 17:11:08 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
@@ -1,20 +1,10 @@
 # 28295 체육은 코딩과목 입니다
-dir = 'N'
-for i in range(10):
-  n = int(input())
-  if n == 1:
-    if dir == 'N': dir = 'E'
-    elif dir == 'E': dir = 'S'
-    elif dir == 'S': dir = 'W'
-    elif dir == 'W': dir = 'N'
-  elif n == 2:
-    if dir == 'N': dir = 'S'
-    elif dir == 'E': dir = 'W'
-    elif dir == 'S': dir = 'N'
-    elif dir == 'W': dir = 'E'
-  elif n == 3:
-    if dir == 'N': dir = 'W'
-    elif dir == 'E': dir = 'N'
-    elif dir == 'S': dir = 'E'
-    elif dir == 'W': dir = 'S'
-print(dir)
+dirs = "NESW"
+delta = [0, 1, 2, -1]  # n=1,2,3에 대응 (0은 더미)
+
+idx = 0  # N
+for _ in range(10):
+    n = int(input())
+    idx = (idx + delta[n]) % 4
+
+print(dirs[idx])
