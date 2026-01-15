210a1098cb294eaf9553d33e6b6d7c259caa259a
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-01-15 14:15:04 +0900

    [Bronze IV] Title: 당구 좀 치자 제발, Time: 96 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Bronze/32642. 당구 좀 치자 제발/당구 좀 치자 제발.py
+++ 백준/Bronze/32642. 당구 좀 치자 제발/당구 좀 치자 제발.py
@@ -1,11 +1,16 @@
-# 32642 당구 좀 치자 제발 2026-01-15(목) 오후 1:21
 import sys
 input = sys.stdin.readline
+
 N = int(input())
-rain = input().split()
-cnt = total = 0
-for i in range(N):
-  if int(rain[i]) == 1: cnt += 1
-  else: cnt -= 1
-  total += cnt 
-print(total)
+# map으로 미리 숫자로 다 바꿔둡니다. (반복문 안에서 int() 호출 비용 절약)
+rain = list(map(int, input().split()))
+
+cnt = 0
+total_anger = 0  # [수정] sum 대신 충돌 안 나는 이름 사용
+
+for x in rain:
+    # [최적화] if문 삭제 -> 수학적 매핑 (1 -> 1, 0 -> -1)
+    cnt += (x * 2 - 1) 
+    total_anger += cnt
+
+print(total_anger)
