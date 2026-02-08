2234e29556621bc62093a1db19f13f54c9a65099
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-02-09 04:28:26 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int h, m;
+    scanf("%d %d", &h, &m);
+    if (m >= 45) m -= 45;
+    else {
+        if (h == 0) { 
+            h = 23; m += 15;
+        }
+        else {
+            h--; m += 15;
+        }
+    }
+    printf("%d %d\n", h, m);
+    return 0;
+}
