429643abae10b540c31f775e5381fb44de4d91a0
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-02-14 00:32:26 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,6 @@
+# 34813_공통교육과정
+string = input()
+if string[0] == 'F': print("Foundation")
+elif string[0] == 'C': print("Claves")
+elif string[0] == 'V': print('Veritas')
+elif string[0] == 'E': print('Exploration')
