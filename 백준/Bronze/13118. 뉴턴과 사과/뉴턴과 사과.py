e8f636d37e427c2890614f514f01c51957f6063d
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-02-28 07:56:20 +0900

    [Bronze IV] Title: 뉴턴과 사과, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13118. 뉴턴과 사과/뉴턴과 사과.py
@@ -0,0 +1,8 @@
+# 13118 뉴턴과 사과
+a, b, c, d = map(int, input().split())
+x, y, z = map(int, input().split())
+if a == x: print(1)
+elif b == x: print(2)
+elif c == x: print(3)
+elif d == x: print(4)
+else: print(0)
