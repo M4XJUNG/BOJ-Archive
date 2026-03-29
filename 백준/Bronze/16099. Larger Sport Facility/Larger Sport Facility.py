07ff81e5c3f6caee65eb3993a2c55118531b7a6f
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-03-29 15:27:59 +0900

    [Bronze IV] Title: Larger Sport Facility, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/16099. Larger Sport Facility/Larger Sport Facility.py
@@ -0,0 +1,9 @@
+# 16099_Larger Sport Facility
+N = int(input())
+for _ in range(N):
+    lt, wt, le, we = map(int, input().split())
+    area_t = lt * wt 
+    area_e = le * we 
+    if area_t > area_e: print('TelecomParisTech')
+    elif area_t < area_e: print('Eurecom')
+    else: print('Tie')
