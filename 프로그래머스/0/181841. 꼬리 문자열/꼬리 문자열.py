040202c6f6a1f1f5a0dff7c574797cede4e460de
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-06-27 00:17:03 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -0,0 +1,7 @@
+def solution(str_list, ex):
+    answer = ''
+    for st in str_list:
+        if ex not in st:
+            answer += st 
+    return answer 
+    # return ''.join(st for st in str_list if ex not in st)
