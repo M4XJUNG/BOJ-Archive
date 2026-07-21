88274f86037120d751221caefc3011d26ac13e78
M4XJUNG <60457658+M4XJUNG@users.noreply.github.com>
2026-07-21 10:44:42 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -0,0 +1,7 @@
+def solution(sides):
+    # big_num = max(sides)
+    # small_num = min(sides)
+    # result1 = range(big_num - small_num + 1, big_num + 1)
+    # result2 = range(big_num + 1, big_num + small_num)
+    # return len(result1) + len(result2)
+    return min(sides) * 2 - 1
