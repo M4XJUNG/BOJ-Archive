00bb340e9351610c521b73f4ef76a39476d97a7b
Myeonghwan Jung <60457658+M4XJUNG@users.noreply.github.com>
2026-09-05 14:27:35 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,8 +1,4 @@
 def solution(num_list):
-    answer = 0
-    for num in num_list:
-        if num < 0: 
-            answer = num_list.index(num) 
-            break
-        else: answer = -1
-    return answer
+    for i in range(len(num_list)):
+        if num_list[i] < 0: return i
+    else: return -1
